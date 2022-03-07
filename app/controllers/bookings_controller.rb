4a1578a5971a706c129bc85c370dadc14bf8cf0a
njoroge kimani <therealkenyan@gmail.com>
2022-03-07 17:24:44 +1100

    finished views and controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,9 +16,30 @@ class BookingsController < ApplicationController
     end
   end
 
+  def confirm
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    if @booking.valid?
+      @booking.save
+      render 'success'
+    else
+      @booking = Booking.new
+      render 'new'
+    end
+  end
+
   def index
     @bookings = Booking.all
     @my_bookings = @bookings.where(user: current_user)
+  end
+
+  def success
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to event_bookings_path(yacht_id: @booking.event.id)
   end
 
 end
