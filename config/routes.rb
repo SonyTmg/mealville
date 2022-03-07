f62de0bbf2ae5c4ad8d07c18af8335d7fc2ce51f
Siddharth Parekh <parekh.siddharth@gmail.com>
2022-03-07 18:58:19 +1100

    Updated reviews basic views, change to the reviews model, updated with the routes having reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events do
+    resources :bookings
+  end
+
+  resources :bookings, only: [] do
+    resources :reviews, only: %i[new create index]
+  end
+
+  resources :reviews, only: %i[show]
 end
