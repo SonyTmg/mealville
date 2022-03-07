18e154530a209787c3ca1d08bcc96ec0a79d8ddf
njoroge kimani <therealkenyan@gmail.com>
2022-03-07 17:07:05 +1100

    index action and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,5 +3,22 @@ class BookingsController < ApplicationController
     @booking = Booking.new(event_id: params[:event_id])
   end
 
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    if @booking.valid?
+      # redirect_to event_confirm_booking_path(event_id: params[:event_id])
+      render 'new'
+    else
+      @booking = Booking.new
+      flash[:alert] = "Error processing booking. Try again later"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def index
+    @bookings = Booking.all
+    @my_bookings = @bookings.where(user: current_user)
+  end
 
 end
