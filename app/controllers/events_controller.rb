1d2326baee493d6c36bb4cece64a12576b533d14
Siddharth Parekh <parekh.siddharth@gmail.com>
2022-03-08 17:44:31 +1100

    Added the search logic to search by name, desc, location and filter by dates on the home page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,13 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[show]
+
   def index
-    @events = Event.all
+    if params[:query].present?
+      @events = Event.search_by_name_description_and_location(params[:query])
+    else
+      @events = Event.all
+    end
+    filter_by_date if params[:dates].present?
   end
 
   def show
@@ -10,6 +16,13 @@ class EventsController < ApplicationController
   end
 
   private
+
+  def filter_by_date
+    dates = params[:dates].split('to')
+    start_date = Date.parse(dates.first)
+    end_date = Date.parse(dates[1])
+    @events = @events.where(date: start_date..end_date)
+  end
 
   def set_event
     @event = Event.find(params[:id])
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,11 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_description_and_location,
+                  against: %i[name description location],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
