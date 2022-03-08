91a7351aaf8159a46f1c2b7420222e9fa9483983
Sony Tamang <sony123ktm@gmail.com>
2022-03-08 15:34:26 +1100

    Add status to booking model

--- app/controllers/host/bookings_controller.rb
+++ app/controllers/host/bookings_controller.rb
@@ -3,4 +3,11 @@ class Host::BookingsController < ApplicationController
 
   def index
   end
+
+  def confirm
+    @booking = Booking.find(params[:booking_id])
+    @booking.confirmed!
+    flash[:notice] = "Booking confirmed!"
+    redirect_back fallback_location: host_events_path
+  end
 end
