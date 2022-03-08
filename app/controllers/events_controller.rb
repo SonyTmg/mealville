b62074dd7335e22ab771e3c1e17c9339817973dc
Ervina Theresa <ervina.theresa@gmail.com>
2022-03-08 15:15:47 +1100

    geocoding draft

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,12 @@ class EventsController < ApplicationController
   before_action :set_event, only: %i[show]
   def index
     @events = Event.all
+    @markers = @events.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude
+      }
+    end
   end
 
   def show
