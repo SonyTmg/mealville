1d51db95118dd3787d4aa05eabfa1f68fae9f7a2
njoroge kimani <therealkenyan@gmail.com>
2022-03-09 23:17:57 +1100

    added stripe button to views and stripe button function in app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def stripe_url
+    "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{ENV['CLIENT_ID']}&scope=read_write"
+  end
+
+  def stripe_connect_button # add this method
+    link_to stripe_url, class: "btn-stripe-connect" do
+      content_tag :span, "Connect with Stripe"
+    end
+  end
 end
