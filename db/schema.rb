ca62a9ff97de4c6c278979c8aaf3e880c9c373b2
njoroge kimani <therealkenyan@gmail.com>
2022-03-09 21:38:59 +1100

    added stripe fields to user table with successful migration. Created env file for stripe api keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_08_033602) do
+ActiveRecord::Schema.define(version: 2022_03_09_103522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,10 @@ ActiveRecord::Schema.define(version: 2022_03_08_033602) do
     t.string "uid"
     t.string "avatar_url"
     t.boolean "host", default: false
+    t.string "stripe_uid"
+    t.string "provider"
+    t.string "access_code"
+    t.string "publishable_key"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
