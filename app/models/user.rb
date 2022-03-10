17e959753e797e8159fabff0da06ff7458f2b2ec
njoroge kimani <therealkenyan@gmail.com>
2022-03-11 09:31:41 +1100

    dropped db to reset migrations and added merchant func to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   pay_customer
+  pay_merchant
   attr_accessor :skip_password_validation
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
