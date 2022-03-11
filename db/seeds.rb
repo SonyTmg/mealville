02e34bbfcf59cca872f8db6e7c48fff0565ee727
Ervina Theresa <ervina.theresa@gmail.com>
2022-03-11 14:59:00 +1100

    Implemented Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,14 @@ images = [
   'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
   'https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vZHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60',
   'https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Zm9vZHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2R8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+  'https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2R8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+  'https://c4.wallpaperflare.com/wallpaper/690/564/924/meat-4k-hd-for-desktop-wallpaper-preview.jpg',
+  'https://images.unsplash.com/photo-1588644525273-f37b60d78512?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmVwYWxpJTIwZm9vZHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  'https://images.unsplash.com/photo-1593252719532-53f183016149?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmVwYWxpJTIwZm9vZHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  'https://images.squarespace-cdn.com/content/v1/53a05cdee4b0c1bc44841a7b/1542172071935-2BDLZXWL2JNBIPYURZSV/101+Food+Photography+Tips',
+  'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+  'https://images.unsplash.com/photo-1432139509613-5c4255815697?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=985&q=80',
+  'https://images.unsplash.com/photo-1565299507177-b0ac66763828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1022&q=80'
 ]
 
 puts "Creating events"
