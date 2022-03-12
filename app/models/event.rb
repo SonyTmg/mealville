fc34764cfacc13b2ffab4efc4f74127e296b15b8
Sony Tamang <sony123ktm@gmail.com>
2022-03-12 20:30:29 +1100

    Add validations to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,19 @@ class Event < ApplicationRecord
   has_many :reviews, through: :bookings
   has_many_attached :photos
 
+  validates :name,
+            :cuisine,
+            :price,
+            :description,
+            :capacity,
+            :location,
+            :date,
+            :start_time,
+            :end_time,
+            :photos, presence: true
+
+  validate :event_date_cannot_be_in_the_past
+  validate :end_time_cannot_be_before_start_time
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
@@ -33,5 +46,15 @@ class Event < ApplicationRecord
 
   def formatted_end_time
     end_time.strftime("%I:%M %p")
+  end
+
+  def event_date_cannot_be_in_the_past
+    errors.add(:date, "can't be in the past") if date.present? && date < Date.today
+  end
+
+  def end_time_cannot_be_before_start_time
+    if (start_time.present? && end_time.present?) && (end_time < start_time)
+      errors.add(:end_time, "can't before start time")
+    end
   end
 end
