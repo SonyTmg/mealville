fdb624928fb880ab58df7a088c7b1886b0afae32
Sony Tamang <sony123ktm@gmail.com>
2022-03-12 20:55:13 +1100

    Host cannot book on their own events

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,8 @@ class Booking < ApplicationRecord
 
   enum status: [:pending, :confirmed, :cancelled], _default: "pending"
 
+  validate :host_cannot_book_their_own_event
+
   # validate :ensure_total_guests_not_exceed_capacity
 
   # def ensure_total_guests_not_exceed_capacity
@@ -12,4 +14,10 @@ class Booking < ApplicationRecord
   #     errors.add(:noguest, "cannot exceed maximum capacity")
   #   end
   # end
+
+  def host_cannot_book_their_own_event
+    if event.user_id == user.id
+      erros.add("You cannot book in your own events")
+    end
+  end
 end
