8d9e4a0177aa5ac429dc0c1ab029f37ea83aa12a
Sony Tamang <sony123ktm@gmail.com>
2022-03-13 16:24:00 +1100

    Remove booking has_many reviews

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :event
   belongs_to :user
-  has_many :reviews, dependent: :destroy
 
   enum status: [:pending, :confirmed, :cancelled], _default: "pending"
 
