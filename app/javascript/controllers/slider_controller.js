916837e25fca0ea9079f34cc7b7d611400832703
Sony Tamang <sony123ktm@gmail.com>
2022-03-13 14:15:12 +1100

    Add slider to home page

---
+++ app/javascript/controllers/slider_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "stimulus"
+import Splide from '@splidejs/splide';
+
+export default class extends Controller {
+  static targets = [ "slider" ]
+
+  connect() {
+    new Splide(this.sliderTarget, {
+      type: 'fade',
+      rewind: true,
+      speed: 2000,
+      autoplay: true,
+      interval: 3000,
+      arrows: false,
+      pagination: false,
+      pauseOnHover: false,
+      easing: 'ease-in-out'
+    }).mount()
+  }
+}
