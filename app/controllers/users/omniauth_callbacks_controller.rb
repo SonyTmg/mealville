837ce337aaf883559fb7657155286189a7ade3a4
Sony Tamang <sony123ktm@gmail.com>
2022-03-17 19:43:04 +1100

    Redirect back to where user was

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_google(**from_google_params)
 
     if user.present?
+      stored_location = stored_location_for(:user)
       sign_out_all_scopes
+      store_location_for(:user, stored_location)
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect user, event: :authentication
     else
@@ -14,11 +16,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   protected
 
-  def after_omniauth_failure_path_for(_scope)
+  def after_omniauth_failure_path_for(resource_or_scope)
     new_user_session_path
   end
 
   def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+      return request.env['omniauth.origin']
+    end
     stored_location_for(resource_or_scope) || root_path
   end
 
